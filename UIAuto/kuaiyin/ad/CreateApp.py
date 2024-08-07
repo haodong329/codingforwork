9f6aea4ff73151bfea6948d2e1076c2bc43bb759
haodongl <Lhdauth123>
2024-08-07 11:06:03 +0800

    添加测试代码

--- UIAuto/kuaiyin/ad/CreateApp.py
+++ UIAuto/kuaiyin/ad/CreateApp.py
@@ -13,8 +13,7 @@ driver=webdriver.Chrome(service=service)
 # 设置元素隐形等待时间
 driver.implicitly_wait(20)
 
-# 打开浏览器,进入快音聚合后台
-driver.get("http://adv-op.rd.kaixinyf.cn/#/dashboard")
+
 # driver.maximize_window()# 浏览器最大化
 # 进行登陆操作
 driver.find_element(By.XPATH,'//*[@id="input-18"]').send_keys('18582897286')
@@ -23,28 +22,16 @@ driver.find_element(By.XPATH,'//*[@id="app-container"]/div/div/div[2]/div/div[2]
 # 进入APP 产品管理
 driver.find_element(By.XPATH,'//*[@id="app"]/section/aside/ul/li[3]').click()
 
-# 获取系统已存在的appid，防止appid重复导致新建失败
-# appid_list=[]
-# app_num=1
-# while 1 :
-#     try:
-#         appid=driver.find_element(By.XPATH,'//*[@id="app"]/section/section/main/div/div[1]/div[2]/div[1]/div[3]/table/tbody/tr['+str(app_num)+']/td[1]/div').text
-#         if appid:
-#             appid_list.append(appid)
-#             app_num = app_num + 1
-#     except Exception as e:
-#         print(e)
-#         break
-
 # 查找所有分页中的appid
 appid_list=[]
 # 先把第一页的appid添加进去
 apps = driver.find_elements(By.XPATH, "//*[@class='el-table_1_column_1 is-center  el-table__cell']")
 for i in apps:
     appid_list.append(i.text)
+# 如果有分页数据，把分页的appid进行添加
 while 1:
     if driver.find_element(By.XPATH,'''//*[@class="btn-next"]''').is_enabled():
-        driver.find_element(By.XPATH, '//*[@id="app"]/section/section/main/div/div[1]/div[2]/div[2]/div/button[2]').click()
+        driver.find_element(By.XPATH, '//*[@class="btn-next"]').click()
         time.sleep(10) #需要添加一个等待时间，翻页有一个加载时间
         apps = driver.find_elements(By.XPATH, "//*[@class='el-table_1_column_1 is-center  el-table__cell']")
         if apps:
@@ -52,33 +39,31 @@ while 1:
                 appid_list.append(i.text)
         else:
             print('没有元素了添加什么！！！')
-
     else:
-        print('没有下一页了！！！')
+        print('没有下一页，不需要进行翻页操作，已完成appid的查找！')
         break
 
-# 初始化自动化的appid命名
+# 初始化自动化的appid命名，防止与已经存在的appid重复
 app_id=80001
-
-# 创建app
-# driver.find_element(By.XPATH,'//*[@id="app"]/section/aside/ul/li[3]').click()
-driver.find_element(By.XPATH,'//*[@id="app"]/section/section/main/div/div[1]/div[2]/form/div/div/button/span').click()
-driver.find_element(By.XPATH,'//*[@id="app"]/section/section/main/div/div[2]/div/div/div[2]/form/div[1]/div/div/input').send_keys('UI自动化测试建立')
-
 while 1:
     if app_id in [int(i) for i in appid_list]:
         app_id = app_id + 1
     else:
         break
+# 创建app
+driver.find_element(By.XPATH,'//*[@class="el-form-item__content"]/button').click()
+driver.find_element(By.XPATH,'//*[@id="app"]/section/section/main/div/div[2]/div/div/div[2]/form/div[1]/div/div/input').send_keys('UI自动化测试建立')
 driver.find_element(By.XPATH,'//*[@id="app"]/section/section/main/div/div[2]/div/div/div[2]/form/div[2]/div/div/input').send_keys(app_id)
-
-
 driver.find_element(By.XPATH,'//*[@id="app"]/section/section/main/div/div[2]/div/div/div[2]/form/div[3]/div/div/div/div/input').click()
-# driver.find_element(By.XPATH,'//*[@id="el-popper-6218"]/div[1]/div/div[1]/ul/li[2]').click()
+time.sleep(5)
+driver.find_element(By.XPATH,'//*[@id="el-popper-4476"]/div[1]/div/div[1]/ul/li[2]/span').click()
 driver.find_element(By.XPATH,'//*[@id="app"]/section/section/main/div/div[2]/div/div/div[2]/form/div[4]/div/div[1]/input').send_keys('com.test.cn')
 driver.find_element(By.XPATH,'//*[@id="app"]/section/section/main/div/div[2]/div/div/div[3]/div/button[2]/span').click()
 
-time.sleep(100)
+
+
+
+time.sleep(60)
 
 
 # # 定位元素
@@ -89,35 +74,6 @@ time.sleep(100)
 #     except TimeoutException as e:
 #         logging.info('定位元素失败 %s' %e)
 #         raise Exception('元素定位失败,请检查元素路径！！！')
-
-# 定位元素
-# def find_element2(driver=driver,timeout=20,poll_frequency=0.5,path=str):
-#     # try:
-#     return WebDriverWait(driver,timeout=timeout,poll_frequency=poll_frequency).\
-#         until(EC.presence_of_element_located(By.XPATH),path)
-#     # except TimeoutException as e:
-#     #     logging.info('定位元素失败 %s' %e)
-#     #     raise Exception('元素定位失败,请检查元素路径！！！')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 def find_element2(self, timeout=10, poll_frequency=0.5, **kwargs):
     """
